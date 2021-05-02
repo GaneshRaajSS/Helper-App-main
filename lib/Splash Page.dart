4f694df4bb42e6aedc9295637d4c7557e2483a44
Ganeshraaj1 <ssganeshraaj@gmail.com>
2021-05-02 12:34:18 +0530

    splash screen

--- lib/Splash Page.dart
+++ lib/Splash Page.dart
@@ -3,8 +3,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'HomeScreen.dart';
 import 'dart:async';
 
-import 'package:shimmer/shimmer.dart';
-
 void main() {
   runApp(
     MaterialApp(
@@ -42,43 +40,6 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Center(
-            child: Container(
-                child: Image.network(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2PiomuOB6jML1Sd_42bkpvcsLZtmmRs0LdQ&usqp=CAU')),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(0),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  colors: [Colors.indigoAccent, Colors.cyan[100]],
-                ),
-              ),
-              child: Shimmer.fromColors(
-                baseColor: Color(0xff7f00ff),
-                highlightColor: Color(0xffe100ff),
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      'Helper',
-                      style: TextStyle(
-                          fontSize: 90.0,
-                          fontFamily: 'Dancing Script',
-                          fontWeight: FontWeight.w900,
-                          shadows: <Shadow>[
-                            Shadow(
-                                blurRadius: 18.0,
-                                color: Colors.black87,
-                                offset: Offset.fromDirection(120, 12))
-                          ]),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
           Container(
             child: Padding(
               padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
